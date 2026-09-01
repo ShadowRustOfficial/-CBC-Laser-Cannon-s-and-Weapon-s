2980e188a9e3d6724f77fdcce4b1e376244ba8d6
ShadowOfficial <leightonsaxty@gmail.com>
2026-09-01 05:45:58 +0100

    Build 15: fix Create ScrollValueBehaviour API and isolate colour control

--- src/main/java/com/example/colorcannons/mount/ColorModeValueBehaviour.java
+++ src/main/java/com/example/colorcannons/mount/ColorModeValueBehaviour.java
@@ -8,6 +8,7 @@ import com.simibubi.create.foundation.blockEntity.behaviour.CenteredSideValueBox
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueBoxTransform;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueSettingsBoard;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueSettingsFormatter;
+import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
@@ -15,48 +16,40 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
-/** Create value control for the mount's colour selector. */
-public class ColorModeValueBehaviour extends com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour {
+/**
+ * Dedicated Create scroll control for the laser colour.
+ *
+ * Values are deliberately limited to two discrete states:
+ * 0 = BLUE, 1 = RED.
+ * The transform has its own hitbox so it cannot overlap CBC's pitch control.
+ */
+public class ColorModeValueBehaviour extends ScrollValueBehaviour {
     public static final BehaviourType<ColorModeValueBehaviour> TYPE = new BehaviourType<>();
     private final ValueBoxTransform slotPositioning;
 
     public ColorModeValueBehaviour(SmartBlockEntity be) {
-        super(be);
-        setLabel(Component.translatable("colorcannons.fe_fixed_cannon_mount.color_mode"));
-        slotPositioning = new ColorModeValueBox();
+        super(Component.translatable("colorcannons.fe_fixed_cannon_mount.color_mode"), be, new ColorModeValueBox());
+        this.slotPositioning = getSlotPositioning();
         between(0, 1);
         withFormatter(v -> ModColorModes.byOrdinalSafe(v).name());
     }
 
-    public ModColorModes getColorMode() { return ModColorModes.byOrdinalSafe(getValue()); }
-
-    @Override
-    public boolean testHit(Vec3 hit) {
-        LevelAccessor level = blockEntity.getLevel();
-        BlockPos pos = blockEntity.getBlockPos();
-        BlockState state = blockEntity.getBlockState();
-        Vec3 localHit = hit.subtract(Vec3.atLowerCornerOf(pos));
-        return slotPositioning.testHit(level, pos, state, localHit);
+    public ModColorModes getColorMode() {
+        return ModColorModes.byOrdinalSafe(getValue());
     }
 
     @Override
-    public MutableComponent formatValue(ValueSettings settings) {
-        ModColorModes mode = ModColorModes.byOrdinalSafe(settings.value());
-        return Component.literal(mode.name()).withStyle(mode == ModColorModes.RED ? ChatFormatting.RED : ChatFormatting.BLUE);
-    }
-
-    @Override
-    public void setValueSettings(Player player, ValueSettings valueSetting, boolean ctrlHeld) {
-        if (!valueSetting.equals(getValueSettings())) playFeedbackSound(this);
-        setValue(valueSetting.value() == 1 ? 1 : 0);
-        blockEntity.setChanged();
-        blockEntity.sendData();
+    public void setValueSettings(Player player, ValueSettings valueSetting, boolean ctrlDown) {
+        int value = valueSetting.value() <= 0 ? ModColorModes.BLUE.ordinal() : ModColorModes.RED.ordinal();
+        if (value == getValue())
+            return;
+        setValue(value);
+        playFeedbackSound(this);
     }
 
     @Override
@@ -64,31 +57,51 @@ public class ColorModeValueBehaviour extends com.simibubi.create.foundation.bloc
         ImmutableList<Component> rows = ImmutableList.of(
                 Component.literal("BLUE").withStyle(ChatFormatting.BLUE),
                 Component.literal("RED").withStyle(ChatFormatting.RED));
-        return new ValueSettingsBoard(label, 1, 1, rows, new ValueSettingsFormatter(this::formatValue));
+        return new ValueSettingsBoard(label, 1, 1, rows,
+                new ValueSettingsFormatter(settings -> formatValue(settings)));
+    }
+
+    private MutableComponent formatValue(ValueSettings settings) {
+        ModColorModes mode = ModColorModes.byOrdinalSafe(settings.value());
+        return Component.literal(mode.name())
+                .withStyle(mode == ModColorModes.RED ? ChatFormatting.RED : ChatFormatting.BLUE);
     }
 
     @Override
-    public ValueBoxTransform getSlotPositioning() { return slotPositioning; }
+    public ValueBoxTransform getSlotPositioning() {
+        return slotPositioning;
+    }
+
     @Override
-    public BehaviourType<?> getType() { return TYPE; }
+    public BehaviourType<?> getType() {
+        return TYPE;
+    }
 
     @Override
     public void write(CompoundTag nbt, HolderLookup.Provider registries, boolean clientPacket) {
+        super.write(nbt, registries, clientPacket);
         nbt.putInt("ColorMode", ModColorModes.byOrdinalSafe(getValue()).ordinal());
     }
 
     @Override
     public void read(CompoundTag nbt, HolderLookup.Provider registries, boolean clientPacket) {
-        setValue(nbt.contains("ColorMode") ? nbt.getInt("ColorMode") : ModColorModes.BLUE.ordinal());
+        super.read(nbt, registries, clientPacket);
+        if (nbt.contains("ColorMode"))
+            setValue(nbt.getInt("ColorMode"));
+        else
+            setValue(ModColorModes.BLUE.ordinal());
     }
 
     private static class ColorModeValueBox extends CenteredSideValueBoxTransform {
         ColorModeValueBox() {
-            super((state, dir) -> state.getValue(BlockStateProperties.FACING) != dir);
+            super((state, direction) -> state.getValue(BlockStateProperties.FACING) != direction);
         }
+
         @Override
         protected Vec3 getSouthLocation() {
-            return new Vec3(8.0 / 16.0, 8.0 / 16.0, 14.0 / 16.0);
+            // Keep this control on the dedicated colour face and away from
+            // CBC's pitch/yaw bearing control hitbox.
+            return new Vec3(8.0 / 16.0, 8.0 / 16.0, 15.5 / 16.0);
         }
     }
 }
